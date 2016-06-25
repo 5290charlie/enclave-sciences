838e7d9aa6c93ada3eb222a8262633affbd67810
Charlie McClung <charlie@cmr1.com>
2016-06-24 19:54:03 -0600

    Added jquery mobile and swipe support

---
+++ static/js/site.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#carousel-main").swiperight(function() {
+      $("#carousel-main").carousel('prev');
+    });
+   $("#carousel-main").swipeleft(function() {
+      $("#carousel-main").carousel('next');  
+   });
+});
